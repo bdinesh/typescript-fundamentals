25b671fa5f79356f1daf0d3d254639c832693dc8
Dinesh Bogolu <visitdinu@gmail.com>
2017-02-13 01:39:19 +0530

    Add TypeScript types

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,24 @@
-function generateRandomId(){
-    return `#${Math.random().toString(36).substr(2,7)}`;
+type ValidSymbol = "#" | "$";
+
+interface RandomIdConfig{
+    length: number,
+    symbol: ValidSymbol
 }
 
-function main(){
-    var appComponent = document.getElementById('app');
+function generateRandomId(options: RandomIdConfig): string {
+    return `${options.symbol}${Math.random()
+        .toString(36)
+        .substr(2, options.length)}`;
+}
+
+function main() {
+    var app = document.getElementById('app');
     setInterval(() => {
-        appComponent.innerHTML = generateRandomId();
+        if (app) {
+            app.innerHTML = generateRandomId({
+                symbol: '#',
+                length: 7
+            });
+        }
     }, 1000);
 }
