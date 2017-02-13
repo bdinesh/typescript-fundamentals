dbfd6cb5f39fffe952d2a69301cbf1db804a51bd
Dinesh Bogolu <visitdinu@gmail.com>
2017-02-13 23:55:32 +0530

    Implement function overloading to generateRandomId function

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,15 @@ interface RandomIdConfig{
     symbol: ValidSymbol
 }
 
-function generateRandomId(options: RandomIdConfig): string {
+function generateRandomId(symbol: ValidSymbol, length: number): string
+function generateRandomId(options: RandomIdConfig): string
+function generateRandomId(options: RandomIdConfig | ValidSymbol): string {
+    if(typeof options === 'string'){
+        return `${options}${Math.random()
+            .toString(36)
+            .substr(2, length)}`;
+    }
+    
     return `${options.symbol}${Math.random()
         .toString(36)
         .substr(2, options.length)}`;
