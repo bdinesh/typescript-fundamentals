817182f42c7bd9fc9720e4c227a402b0bd038f00
Dinesh Bogolu <visitdinu@gmail.com>
2017-02-21 00:49:57 +0530

    Implement class and method decorators

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,43 @@
 import { generateRandomId } from './utils';
 
-function main() {
-    var app = document.getElementById('app');
-    setInterval(() => {
-        if (app) {
-            app.innerHTML = generateRandomId({
-                symbol: '#',
-                length: 7
-            });
-        }
-    }, 1000);
+function Component(options: { id: string }) {
+    return target => {
+        target.id = options.id;
+    };
 }
 
-main();
+function enumerable(isEnumerable: boolean) {
+    return (target, propertyKey, propertyDescriptor: PropertyDescriptor) => {
+        console.log("propertyKey: ", propertyKey);
+        propertyDescriptor.enumerable = isEnumerable;
+    };
+}
+
+@Component({
+    id: 'app'
+})
+class App {
+    @enumerable(false)
+    init(element: HTMLElement | null): void {
+        setInterval(() => {
+            if (element) {
+                element.innerHTML = generateRandomId({
+                    symbol: '#',
+                    length: 7
+                });
+            }
+        }, 1000);
+    }
+}
+
+for(let key in App.prototype){
+    console.log(key);
+}
+
+function main(ComponentClass) {
+    const element = document.getElementById(ComponentClass.id);
+    const cmp = new ComponentClass();
+    cmp.init(element);
+}
+
+main(App);
